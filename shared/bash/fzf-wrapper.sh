8458a807a90e2c0504d4de73cfbd4ba3a2e3becf
Olaf Alders <olaf@wundersolutions.com>
2021-04-27 13:33:39 -0400

    Add fzf wrapper f()

---
+++ shared/bash/fzf-wrapper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+export FZF_DEFAULT_OPTS='--multi --pointer ">>"'
+
+f () {
+    fzf --bind='ctrl-/:toggle-preview' --preview "bat --style=numbers --color=always --line-range :500 {}" "$@"
+}
