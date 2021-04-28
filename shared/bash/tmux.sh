e2e37584f1da337a4efb739656fb664fc7ac72e9
Olaf Alders <olaf@wundersolutions.com>
2021-04-28 12:10:32 -0400

    Don't use eval when starting a tmux session
    This gets rid of "-bash: syntax error near unexpected token `from'" when
    detaching from the tmux session.

--- shared/bash/tmux.sh
+++ shared/bash/tmux.sh
@@ -1,4 +1,6 @@
+# Can't add this as a fzf completion for tmux as I need tmux itself to get a
+# list of the running sessions.
 tm() {
     SESSION=$(tmux list-session | cut -d' ' -f1 | fzf)
-    eval "$(tmux attach "$@" -t "$SESSION")"
+    tmux attach "$@" -t "$SESSION"
 }
